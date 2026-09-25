30a9edd8c42508c4969467b4a6b77c7661ede3b2
Victor Tikhonov <victor.tix777@mail.ru>
2026-09-25 21:34:53 +0400

    Добавлен сервер для gRPC
    - изменен билд
    - создан .proto файл
    - написан сервер gRPC
    - написан main

---
+++ server_1_1/src/main/kotlin/task_3/Main_3.kt
@@ -0,0 +1,32 @@
+package ru.tikhonov.task_3
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.grpc.ServerBuilder
+import kotlin.concurrent.thread
+
+private val logger = KotlinLogging.logger {}
+
+fun main() {
+    val port = 8083
+
+    val server = ServerBuilder
+        .forPort(port)
+        .addService(EchoServiceImpl())
+        .build()
+        .start()
+
+    logger.info { "gRPC server started on port $port" }
+
+    val serverThread = thread(isDaemon = true, name = "grpc-server") {
+        server.awaitTermination()
+    }
+
+    logger.info { "Press ENTER to stop the server" }
+    readln()
+
+    logger.info { "Stopping..." }
+    server.shutdown()
+    server.awaitTermination()
+    serverThread.join()
+    logger.info { "gRPC server stopped" }
+}
