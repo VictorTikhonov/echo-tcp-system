dc952657f30de1d8cc0ddbe293eb3f1a5c504b2c
Victor Tikhonov <victor.tix777@mail.ru>
2026-09-25 21:43:08 +0400

    Рефакторинг
    - поправлен main клиента для сокетов
    - удалены неиспользующиеся классы

--- client_1_1/src/main/kotlin/task_1/Main.kt
+++ client_1_1/src/main/kotlin/task_1/Main.kt
@@ -1,29 +1,6 @@
 package ru.tikhonov.task_1
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import ru.tikhonov.utils.startBlockingBenchmark
-
-private val logger = KotlinLogging.logger {}
-
-//fun main() {
-//
-//    val client = try {
-//        TcpClient(
-//            host = "localhost",
-//            port = 8081
-//        )
-//    } catch (e: IOException) {
-//        logger.warn { "Не удалось подключиться к серверу: ${e.message}" }
-//        return
-//    }
-//
-//    try {
-//        val benchmarkService = BenchmarkService(client)
-//        benchmarkService.start(5)
-//    } finally {
-//        client.close()
-//    }
-//}
 
 fun main() {
 
--- client_1_1/src/main/kotlin/utils/BenchmarkRunnerInterface.kt
+++
@@ -1,5 +0,0 @@
-package ru.tikhonov.utils
-
-interface BenchmarkRunnerInterface {
-    fun runBenchmark(countTcpClient: Int, messagesPerClient: Int)
-}
